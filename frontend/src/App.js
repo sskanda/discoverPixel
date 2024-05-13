a48fa15b46e431b22c69b6fa595d704d82d00889
Skanda <sskanda98@gmail.com>
2024-05-14 05:10:24 +0530

    add few routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,24 @@
 import logo from "./logo.svg";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import "./App.css";
+import Users from "./user/pages/Users";
+import NewPlaces from "./places/pages/NewPlaces";
 
 function App() {
-  return <div className="App">Lets start</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Users />}></Route>
+        <Route path="/places/new" element={<NewPlaces />}></Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ frontend/src/places/pages/NewPlaces.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NewPlaces = () => {
+  return <div>NewPlaces</div>;
+};
+
+export default NewPlaces;
