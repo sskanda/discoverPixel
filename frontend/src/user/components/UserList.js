e9f8d9db638978c6ec88687dc45dd321eeb37cdb
Skanda <sskanda98@gmail.com>
2024-05-18 23:51:19 +0530

    style and add user content

--- frontend/src/user/components/UserList.js
+++ frontend/src/user/components/UserList.js
@@ -1,14 +1,26 @@
 import React from "react";
 import UserItem from "./UserItem";
-
+import "./UserList.css";
 const UserList = (props) => {
-  if (props.items.lenght === 0) return <div>No Users</div>;
+  if (props.items.length === 0) {
+    return (
+      <div className="center">
+        <h2>No users found.</h2>
+      </div>
+    );
+  }
 
   return (
-    <ul>
-      {props.items.map((user) => {
-        return <UserItem key={user.id} name={user.name}></UserItem>;
-      })}
+    <ul className="users-list">
+      {props.items.map((user) => (
+        <UserItem
+          key={user.id}
+          id={user.id}
+          image={user.image}
+          name={user.name}
+          placeCount={user.places}
+        />
+      ))}
     </ul>
   );
 };
