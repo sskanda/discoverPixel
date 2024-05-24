1b5ef13d26b15cbe11f741a753fda976d881cf08
Skanda <sskanda98@gmail.com>
2024-05-25 04:13:49 +0530

    added modal window

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -11,8 +11,8 @@ const Users = () => {
       places: 3,
     },
     {
-      id: "u1",
-      name: "Ron Goku",
+      id: "u2",
+      name: "aRn Gokus",
       image:
         "https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
       places: 3,
