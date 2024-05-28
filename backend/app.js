54efb3a62b31cd20533a923873fc1af3fdb2197c
Skanda <sskanda98@gmail.com>
2024-05-29 04:06:03 +0530

    change mongoose version

--- backend/app.js
+++ backend/app.js
@@ -25,14 +25,18 @@ app.use((error, req, res, next) => {
   res.status(error.code || 500);
   res.json({ message: error.message || "An unknown error occurred!" });
 });
+const uri =
+  "mongodb+srv://skz:admin@cluster0.c7xj5qh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const clientOptions = {
+  serverApi: { version: "1", strict: true, deprecationErrors: true },
+};
 
 mongoose
-  .connect(
-    "mongodb+srv://skz:admin@cluster0.c7xj5qh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-  )
+  .connect(uri, clientOptions)
   .then(() => {
     app.listen(5000);
   })
   .catch((err) => {
-    console.log(err);
+    console.log("ERROR" + err);
   });
