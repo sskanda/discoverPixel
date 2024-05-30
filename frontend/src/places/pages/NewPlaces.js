d043c5948ced5a607baeb971e3c9ad7fc5b323e7
Skanda <sskanda98@gmail.com>
2024-05-30 16:48:44 +0530

    new places form

--- frontend/src/places/pages/NewPlaces.js
+++ frontend/src/places/pages/NewPlaces.js
@@ -1,7 +1,14 @@
 import React from "react";
 
-const NewPlaces = () => {
-  return <div>NewPlaces</div>;
+import Input from "../../shared/components/FormElements/Input";
+import "./NewPlace.css";
+
+const NewPlace = () => {
+  return (
+    <form className="place-form">
+      <Input element="input" type="text" label="Title" />
+    </form>
+  );
 };
 
-export default NewPlaces;
+export default NewPlace;
