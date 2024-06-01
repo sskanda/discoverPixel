89873d26b649e906d3753ca43a403b69b753f99f
Skanda <sskanda98@gmail.com>
2024-06-02 04:54:07 +0530

    add auth page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,15 @@
-import logo from "./logo.svg";
 import {
   BrowserRouter as Router,
   Route,
   Routes,
   Navigate,
 } from "react-router-dom";
-import "./App.css";
 import Users from "./user/pages/Users";
 import NewPlaces from "./places/pages/NewPlaces";
 import UserPlaces from "./places/pages/UserPlaces";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
 import UpdatePlace from "./places/pages/UpdatePlace";
+import Auth from "./user/pages/Auth";
 
 function App() {
   return (
@@ -22,6 +21,7 @@ function App() {
           <Route path="/:userId/places" element={<UserPlaces />}></Route>
           <Route path="/places/new" element={<NewPlaces />}></Route>
           <Route path="/places/:placeId" element={<UpdatePlace />}></Route>
+          <Route path="/auth" element={<Auth />}></Route>
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </main>
